c510fbed2a5dd5f1348a9d67547407e713d4dbc3
jirwal001 <jiri.waloschek@agel.cz>
2025-07-20 13:27:10 +0200

    Update vyzkumna_otazka_5.sql

--- vyzkumna_otazka_5.sql
+++ vyzkumna_otazka_5.sql
@@ -1,3 +1,8 @@
+----- Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo následujícím roce výraznějším růstem?
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
 create temporary table tmp_5 as
 with mzdy as(   
 select 
