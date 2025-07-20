ad6615445aec76eef276b5f6536a89ca7e58209f
jirwal001 <jiri.waloschek@agel.cz>
2025-07-20 13:25:57 +0200

    Update vyzkumna_otazka_5.sql

--- vyzkumna_otazka_5.sql
+++ vyzkumna_otazka_5.sql
@@ -1,1 +1,38 @@
+create temporary table tmp_5 as
+with mzdy as(   
+select 
+payroll_year as rok,
+round (avg (value)) as prumer_mzda,
+lag (avg (value)) over (order by (payroll_year)) as prumer_mzda_loni,
+(round (avg (value)) - lag (avg (value)) over (order by (payroll_year))) as rozdilmezd,
+round(((avg (value)-lag (avg (value)) over (order by (payroll_year)))/lag (avg (value)) over (order by (payroll_year)) * 100)::numeric, 2) as procenta_mzdy
+from czechia_payroll cp 
+where value_type_code = 5958 and calculation_code = 200 
+group by payroll_year
+order by payroll_year 
+), potraviny as
+(
+select 
+date_part ('year', date_from) as rok2,
+avg (value) as prum_cen_potr,
+lag (avg(value)) over (order by (date_part ('year', date_from))) as prum_cen_potr_loni,
+round(((avg(value)-lag (avg(value)) over (order by (date_part ('year', date_from))))/lag (avg(value)) over (order by (date_part ('year', date_from))) * 100)::numeric, 2) as procenta_potr
+from czechia_price cp 
+group by rok2
+order by rok2
+)
+select 
+*,
+procenta_potr - procenta_mzdy as rozdil_procent
+from mzdy
+join potraviny on rok2 = rok;
 
+
+select
+tmp_5.rok,
+tmp_5.procenta_mzdy as vyvoj_mezd_proc,
+tmp_5.procenta_potr as vyvoj_potravin_proc,
+round(((gdp - (lag(gdp) over (order by year asc)))/(lag(gdp) over (order by year asc)) * 100)::numeric, 2) as vyvoj_gdp_proc
+from economies e 
+join tmp_5 on e."year" = rok
+where country = 'Czech Republic'
