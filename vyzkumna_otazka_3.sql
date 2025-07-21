73ea6c05c7fb17003e3ff1125ea810510d5542f2
jirwal001 <jiri.waloschek@agel.cz>
2025-07-21 10:31:20 +0200

    Update vyzkumna_otazka_3.sql

--- vyzkumna_otazka_3.sql
+++ vyzkumna_otazka_3.sql
@@ -1,7 +1,8 @@
 ----- Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
 -----------------------------------------------------------------------------------------------------
------ Nejpomaleji zdražuje kategorie banány žluté (116103) o 7,4 %
------ Kategorie Cukr krystalový (118101) zlevnila o 27,5 %, a rajská jablka červená kulatá (117101) zlevnila o 23,1 %
+----- Nejpomaleji zdražuje kategorie banány žluté (116103) o 7,4 %.
+----- Kategorie Cukr krystalový (118101) zlevnila o 27,5 %, a rajská jablka červená kulatá (117101) zlevnila o 23,1 %.
+----- Data s cenami produktů jsou k dispozici za roky 2006 až 2018.
 
 with tabulka_potravin as(
 select 
