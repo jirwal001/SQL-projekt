6d93c1e727da120885c43b18c4ee77591bdecdf1
jirwal001 <jiri.waloschek@agel.cz>
2025-07-21 11:24:04 +0200

    Update vyzkumna_otazka_4.sql

--- vyzkumna_otazka_4.sql
+++ vyzkumna_otazka_4.sql
@@ -3,7 +3,7 @@
 ----- Neexistuje. V roce 2017 ceny potravin sice dosáhly téměř 10% růstu (9,98 %), ale růst mezd nebyl výrazně nižší (6,43 %).
 ----- Největší procentuální rozdíl mezi mzdami a cenami potravin byl v roce 2009. Potraviny klesly o 6,8 % a mzdy vzrostly o 3,25 % (nejde zde však o růst cen potravin). 
 ----- Data o mzdách jsou k dispozici za roky 2000 až 2021 a data o potravinách za roky 2006 až 2018. Srovnávané období je tedy 2007 až 2018.
------ Rok 2006 nelze srovnat, nejsou k dispozici kompletní data – chybí data o cenách potravin za předchozí rok 2005. 
+----- Rok 2006 nelze srovnat, nejsou k dispozici data za předchozí rok. 
 
 with mzdy as(   
 select 
