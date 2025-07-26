f7d32f005efab67bdab6eb33d1a127018c1193dd
jirwal001 <jiri.waloschek@agel.cz>
2025-07-26 23:38:37 +0200

    Update vyzkumna_otazka_1.sql

--- vyzkumna_otazka_1.sql
+++ vyzkumna_otazka_1.sql
@@ -5,6 +5,31 @@
 ----- Nejvíce poklesů mezd zaznamenala odvětví: B (Těžba a dobývání) 4, a odvětvím: O (Veřejná správa a obrana; povinné sociální zabezpečení), R (Kulturní, zábavní a rekreační činnosti), I (Ubytování, stravování a pohostinství) poklesla mzda 3x
 ----- Data se mzdami jsou k dispozici za roky 2000 až 2021.
 
+with porovnani_mezd_1 as 
+(
+	select 
+		industry_branch_code,
+		payroll_year,
+		avg (v1),
+		lag (avg (v1)) over (partition by industry_branch_code order by industry_branch_code, payroll_year) as prumerna_mzda_loni,
+		avg (v1) - lag (avg (v1)) over (partition by industry_branch_code order by payroll_year) as mezirocni_rozdil,
+			case 
+				when avg (v1) - lag (avg (v1)) over (partition by industry_branch_code order by payroll_year) > 0 then 'roste' else 'klesa'
+			end as vyvoj
+	from t_jiri_waloschek_projekt_sql_primary_final 
+	group by industry_branch_code, payroll_year 
+	order by industry_branch_code, payroll_year 
+)
+select
+	industry_branch_code as odvetvi,
+	count (vyvoj) as pocet_roste
+from porovnani_mezd_1
+where payroll_year between 2001 and 2021 and vyvoj = 'roste' and industry_branch_code is not null and prumerna_mzda_loni is not null
+group by industry_branch_code
+order by count(vyvoj) desc
+
+-----------------------
+SMAZAT
 with porovnani_mezd_1 as( 
 select 
 industry_branch_code as odvetvi, 
