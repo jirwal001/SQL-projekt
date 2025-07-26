dae909b42ec14e06feaba11bd58115bffd2c16f6
jirwal001 <jiri.waloschek@agel.cz>
2025-07-26 23:36:45 +0200

    Create t_jiri_waloschek_projekt_SQL_secondary_final.sql

---
+++ t_jiri_waloschek_projekt_SQL_secondary_final.sql
@@ -0,0 +1,14 @@
+create table t_jiri_waloschek_projekt_SQL_secondary_final as
+with hdp as
+(
+	select
+		country, 
+		year,
+		gdp,
+		lag(gdp) over (order by (year)),
+		round(((gdp - (lag(gdp) over (order by year asc)))/(lag(gdp) over (order by year asc)) * 100)::numeric, 6) as vyvoj_gdp_proc
+	from economies e 
+	group by year, country, gdp
+	having country = 'Czech Republic'
+)
+select * from hdp
