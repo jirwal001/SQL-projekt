744dee07627244cf87693ce3a87c07c0e9076a54
jirwal001 <jiri.waloschek@agel.cz>
2025-07-26 23:40:05 +0200

    Update vyzkumna_otazka_2.sql

--- vyzkumna_otazka_2.sql
+++ vyzkumna_otazka_2.sql
@@ -4,6 +4,40 @@
 ----- V roce 2006 si bylo možno za průměrnou mzdu pro daný rok koupit 1403 l mléka, v roce 2018 1611 l mléka.
 ----- Data s cenami produktů jsou k dispozici za roky 2006 až 2018 a s příjmy 2001 až 2021. Srovnávané období je tedy 2006 až 2018.
 
+with prum_ceny as
+(
+	select
+		year,
+		category_code,
+		round(avg(value)::numeric, 2) as prum_cena
+		from t_jiri_waloschek_projekt_sql_primary_final 
+	group by year, category_code
+	having category_code IN (111301, 114201) and year in (2006, 2018)
+	order by year
+),
+prum_platy as 
+(
+	select
+		payroll_year,
+		avg(v1) as prum_plat
+		from t_jiri_waloschek_projekt_sql_primary_final 
+	group by payroll_year
+	having payroll_year in (2006, 2018)
+	order by payroll_year 
+)
+select 
+	year as rok,
+	category_code as produkt,
+	prum_cena,
+	prum_plat,
+	round((prum_plat / prum_cena)::numeric, 0) as vysledek
+from prum_platy pp
+join prum_ceny pc on pc.year = pp.payroll_year
+order by pc.category_code 
+
+--------------------------
+  SMAZAT
+
 with PRUM_CENY as( -- PRUM CENY JSOU 2006-2018
 select
 date_part('year', date_from) as year,
