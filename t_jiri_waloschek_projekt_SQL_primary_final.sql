6c1cc65e1b35ad282c48be2871f23d6d8b3be5bf
jirwal001 <jiri.waloschek@agel.cz>
2025-07-27 15:37:05 +0200

    Update t_jiri_waloschek_projekt_SQL_primary_final.sql

--- t_jiri_waloschek_projekt_SQL_primary_final.sql
+++ t_jiri_waloschek_projekt_SQL_primary_final.sql
@@ -5,7 +5,7 @@ with mzdy as
 		payroll_year,
         industry_branch_code,
 		value as v1
-	from czechia_payroll cp
+	from czechia_payroll
 	where value_type_code = '5958' and calculation_code = '100'
 ), prum_ceny as
 (
@@ -13,9 +13,23 @@ with mzdy as
 		date_part('year', date_from) as year,
 		category_code,
 		value
-	from czechia_price cp 
+	from czechia_price 
+), kategorie_odvetvi as
+(
+	select
+		code,
+		name
+	from czechia_payroll_industry_branch c 
+), kategorie_produkt as
+(
+	select
+		code as kod_produktu,
+		name as nazev_produktu
+	from czechia_price_category cpc
 )
 select
 *
-from mzdy
-left join prum_ceny pc on mzdy.payroll_year = pc.year
+from mzdy m
+left join prum_ceny pc on m.payroll_year = pc.year
+left join kategorie_odvetvi on code = industry_branch_code
+left join kategorie_produkt on kod_produktu = category_code
