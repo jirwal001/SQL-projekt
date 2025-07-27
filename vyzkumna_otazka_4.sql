7352df38b46ae835de935f5f1f8212e52fac9fff
jirwal001 <jiri.waloschek@agel.cz>
2025-07-27 15:33:06 +0200

    Update vyzkumna_otazka_4.sql

--- vyzkumna_otazka_4.sql
+++ vyzkumna_otazka_4.sql
@@ -5,31 +5,32 @@
 ----- Data o mzdách jsou k dispozici za roky 2000 až 2021 a data o potravinách za roky 2006 až 2018. Srovnávané období je tedy 2007 až 2018.
 ----- Rok 2006 nelze srovnat, nejsou k dispozici data za předchozí rok. 
 
-with mzdy as(   
-select 
-payroll_year as rok,
-avg (value) as prumer_mzda,
-lag (avg (value)) over (order by (payroll_year)) as prumer_mzda_loni,
-round(((avg (value)-lag (avg (value)) over (order by (payroll_year)))/lag (avg (value)) over (order by (payroll_year)) * 100)::numeric, 2) as procenta_mzdy
-from czechia_payroll cp 
-where value_type_code = 5958 and calculation_code = 100 
-group by payroll_year
-order by payroll_year 
+with mzdy as
+(   
+	select 
+		payroll_year as rok,
+		avg (v1) as prumer_mzda,
+		lag (avg (v1)) over (order by (payroll_year)) as prumer_mzda_loni,
+		round(((avg (v1)-lag (avg (v1)) over (order by (payroll_year)))/lag (avg (v1)) over (order by (payroll_year)) * 100)::numeric, 2) as procenta_mzdy
+	from t_jiri_waloschek_projekt_sql_primary_final
+	--where value_type_code = 5958 and calculation_code = 100 
+	group by payroll_year
+	order by payroll_year 
 ), potraviny as
 (
-select 
-date_part ('year', date_from) as rok2,
-avg (value) as prum_cen_potr,
-lag (avg(value)) over (order by (date_part ('year', date_from))) as prum_cen_potr_loni,
-round(((avg(value)-lag (avg(value)) over (order by (date_part ('year', date_from))))/lag (avg(value)) over (order by (date_part ('year', date_from))) * 100)::numeric, 2) as procenta_potraviny
-from czechia_price cp 
-group by rok2
-order by rok2
+	select 
+		year as rok2,
+		avg (value) as prum_cen_potr,
+		lag (avg(value)) over (order by (year)) as prum_cen_potr_loni,
+		round(((avg(value)-lag (avg(value)) over (order by (year)))/lag (avg(value)) over (order by ((year))) * 100)::numeric, 2) as procenta_potraviny
+	from t_jiri_waloschek_projekt_sql_primary_final
+	group by rok2
+	order by rok2
 )
 select 
-rok,
-procenta_mzdy,
-procenta_potraviny,
-procenta_potraviny - procenta_mzdy as rozdil_procent
+	rok,
+	procenta_mzdy,
+	procenta_potraviny,
+	procenta_potraviny - procenta_mzdy as rozdil_procent
 from mzdy
 join potraviny on rok2 = rok;
