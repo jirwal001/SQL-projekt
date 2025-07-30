c35f0785d7061353d943e0a8866770cd6488fae9
jirwal001 <jiri.waloschek@agel.cz>
2025-07-30 10:13:38 +0200

    Update vyzkumna_otazka_2.sql

--- vyzkumna_otazka_2.sql
+++ vyzkumna_otazka_2.sql
@@ -4,35 +4,36 @@
 ----- V roce 2006 si bylo možno za průměrnou mzdu pro daný rok koupit 1403 l mléka, v roce 2018 1611 l mléka.
 ----- Data s cenami produktů jsou k dispozici za roky 2006 až 2018 a s příjmy 2001 až 2021. Srovnávané období je tedy 2006 až 2018.
 
-with prum_ceny as
+WITH avg_prices AS
 (
-	select
+	SELECT
 		year,
 		category_code,
-		nazev_produktu,
-		round(avg(value)::numeric, 2) as prum_cena
-		from t_jiri_waloschek_projekt_sql_primary_final
-	where category_code IN (111301, 114201) and year in (2006, 2018)
-	group by year, category_code, nazev_produktu	
-	order by year
-),
-prum_platy as 
+		product_name,
+		ROUND(AVG(value)::NUMERIC, 2) AS avg_price
+		FROM t_jiri_waloschek_projekt_sql_primary_final
+	WHERE category_code IN (111301, 114201) 
+		AND year in (2006, 2018)
+	GROUP BY year, category_code, product_name	
+	ORDER BY year
+), avg_wages AS 
 (
-	select
+	SELECT
 		payroll_year,
-		avg(v1) as prum_plat
-		from t_jiri_waloschek_projekt_sql_primary_final
-	where payroll_year in (2006, 2018)
-	group by payroll_year
-	order by payroll_year 
+		AVG(v1) AS avg_wage
+		FROM t_jiri_waloschek_projekt_sql_primary_final
+	WHERE payroll_year in (2006, 2018)
+	GROUP BY payroll_year
+	ORDER BY payroll_year 
 )
-select 
-	year as rok,
-	category_code as kategorie_potravin,
-	nazev_produktu as nazev_kategorie,
-	prum_cena,
-	prum_plat,
-	round((prum_plat / prum_cena)::numeric, 2) as vysledek
-from prum_platy pp
-join prum_ceny pc on pc.year = pp.payroll_year
-order by pc.category_code 
+SELECT 
+	year,
+	category_code,
+	product_name as category_name,
+	avg_price,
+	avg_wage,
+	ROUND((avg_wage / avg_price)::NUMERIC, 2) AS result
+FROM avg_wages awg
+JOIN avg_prices apc 
+	ON apc.year = awg.payroll_year
+ORDER BY category_code 
