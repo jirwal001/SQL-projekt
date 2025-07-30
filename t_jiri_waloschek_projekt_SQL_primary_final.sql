d782f9b30aa6cc54b96ca0ee540c3bcbdbb9e781
jirwal001 <jiri.waloschek@agel.cz>
2025-07-30 10:05:02 +0200

    Update t_jiri_waloschek_projekt_SQL_primary_final.sql

--- t_jiri_waloschek_projekt_SQL_primary_final.sql
+++ t_jiri_waloschek_projekt_SQL_primary_final.sql
@@ -1,35 +1,38 @@
-create table t_jiri_waloschek_projekt_sql_primary_final as
-with mzdy as
+CREATE TABLE t_jiri_waloschek_projekt_sql_primary_final AS
+WITH wages AS
 (
-	select 
+	SELECT 
 		payroll_year,
-        industry_branch_code,
-		value as v1
-	from czechia_payroll
-	where value_type_code = '5958' and calculation_code = '100'
-), prum_ceny as
+        	industry_branch_code,
+		value AS v1
+	FROM czechia_payroll
+	WHERE value_type_code = 5958 AND calculation_code = 100
+), avg_prices AS
 (
-	select
-		date_part('year', date_from) as year,
+	SELECT
+		date_part('year', date_from) AS year,
 		category_code,
 		value
-	from czechia_price 
-), kategorie_odvetvi as
+	FROM czechia_price 
+), industry_branch AS
 (
-	select
+	SELECT
 		code,
 		name
-	from czechia_payroll_industry_branch c 
-), kategorie_produkt as
+	FROM czechia_payroll_industry_branch c 
+), product_category AS
 (
-	select
-		code as kod_produktu,
-		name as nazev_produktu
-	from czechia_price_category cpc
+	SELECT
+		code AS product_code,
+		name AS product_name
+	FROM czechia_price_category cpc
 )
-select
-*
-from mzdy m
-left join prum_ceny pc on m.payroll_year = pc.year
-left join kategorie_odvetvi on code = industry_branch_code
-left join kategorie_produkt on kod_produktu = category_code
+SELECT
+	*
+FROM wages w
+LEFT JOIN avg_prices avp
+	ON w.payroll_year = avp.year
+LEFT JOIN industry_branch idb
+	ON idb.code = w.industry_branch_code
+LEFT JOIN product_category pdc
+	ON pdc.product_code = avp.category_code
